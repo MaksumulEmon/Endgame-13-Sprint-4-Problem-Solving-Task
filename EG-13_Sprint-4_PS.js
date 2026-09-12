ec53ca7aaf3510f99f5f31084acfac4d64bab22a
MaksumulEmon <maksumulemon300@gmail.com>
2026-09-13 01:04:36 +0600

    Permutation in String

--- EG-13_Sprint-4_PS.js
+++ EG-13_Sprint-4_PS.js
@@ -165,16 +165,16 @@ console.log(searchRange([5, 7, 7, 8, 8, 10], 8));
 
 // 09. Permutation in String 
 
-// var checkInclusion = function (s1, s2) {
-//     for (let i = 0; i <= s2.length - s1.length; i++) {
-//         let part = s2.substring(i, i + s1.length);
+var checkInclusion = function (s1, s2) {
+    for (let i = 0; i <= s2.length - s1.length; i++) {
+        let part = s2.substring(i, i + s1.length);
 
-//         if (part.split('').sort().join('') === s1.split('').sort().join('')) {
-//             return true;
-//         }
-//     }
+        if (part.split('').sort().join('') === s1.split('').sort().join('')) {
+            return true;
+        }
+    }
 
-//     return false;
-// };
+    return false;
+};
 
-// console.log(checkInclusion("ab", "eidbaooo"));
+console.log(checkInclusion("ab", "eidbaooo"));
